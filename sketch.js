4df1b966e49818b5ced6d816ea329ac0f753430f
Kharis Spencer <kspencer@wlapcs.org>
2017-03-02 20:20:09 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,23 @@
+function setup(){
+   createCanvas(600,400);
+ background(100,100,100);
+}
+
+function draw(){
+     line(35,80,300,200);
+     
+     stroke(255,255,255);
+    fill(244,66,161);
+    rect(290,100,200,250);
+    
+    stroke(0,0,0);
+    strokeWeight(4);
+    fill(0,0,255);
+    ellipse(50,250,80,80);
+   
+   
+}
+
+
+
+
